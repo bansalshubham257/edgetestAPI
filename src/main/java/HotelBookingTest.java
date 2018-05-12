96f218996f4eec199e98298adc786fb9d3b6c2d4
Shubham <bansalshubham257@gmail.com>
2018-05-12 15:24:11 +0530

    Added Before Test Annotaion to configure chrome driver

--- src/main/java/HotelBookingTest.java
+++ src/main/java/HotelBookingTest.java
@@ -4,11 +4,20 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.Select;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class HotelBookingTest {
 
-    WebDriver driver = new ChromeDriver();
+	 WebDriver driver;
+	    
+	 //...................Adding Before Test Method................
+	    
+   @BeforeTest
+   public void setDriver() {
+    	 setDriverPath();
+    	 driver = new ChromeDriver();
+	}
 
     @FindBy(linkText = "Hotels")
     private WebElement hotelLink;
